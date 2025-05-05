129558d806097fd4aaaaf5fee8740c587c8a260d
EunLee <leee331@ajou.ac.kr>
2025-05-05 17:54:38 +0900

    Merge src directories and unify project structure

--- src/main/java/com/ttuikong/spring/FinalProjectApplication.java
+++ src/main/java/com/ttuikong/spring/FinalProjectApplication.java
@@ -1,10 +1,11 @@
 package com.ttuikong.spring;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@MapperScan("com.ttuikong.spring.model.dao")
 public class FinalProjectApplication {
 
 	public static void main(String[] args) {
